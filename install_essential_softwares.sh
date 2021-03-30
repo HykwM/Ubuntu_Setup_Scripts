98a390feb41d2cfd4572fe8ad5cc380c56f5bd76
HykwM <mst.hayakawa@gmail.com>
2021-03-30 23:07:20 +0900

    色々修正（未検証）

--- install_essential_softwares.sh
+++ install_essential_softwares.sh
@@ -3,9 +3,8 @@ set -Ceu
 
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y anydesk git chromium-browser baobab curl ssh
-sudo ubuntu-drivers autoinstall
-
+sudo apt install -y anydesk git chromium-browser baobab curl ssh nvidia-driver-460
 echo "インストール作業が完了しました。再起動してください。"
--- install_nvidia_docker.sh
+++ install_nvidia_docker.sh
@@ -12,29 +12,16 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 
 
-echo "nvidia-container-toolkitおよびnvidia-container-runtimeをインストールします。
-	参考：https://github.com/NVIDIA/nvidia-container-runtime
-	      https://github.com/NVIDIA/nvidia-docker"
+echo "nvidia-docker2をインストールします。
+	参考：https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#installing-docker-ce"
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add -
-#curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
-
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
-
-sudo tee /etc/docker/daemon.json <<EOF
-{
-    "runtimes": {
-        "nvidia": {
-            "path": "/usr/bin/nvidia-container-runtime",
-            "runtimeArgs": []
-        }
-    }
-}
-EOF
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
 sudo usermod -aG docker $USER
+sudo systemctl restart docker
 
 echo "インストール作業が完了しました。再起動してください。"
