57d69721dc69ebc40a67f30cd4dedd2ede7797fa
HykwM <mst.hayakawa@gmail.com>
2021-04-01 18:00:15 +0900

    enable magic sysrq key

--- install_essential_softwares.sh
+++ install_essential_softwares.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -Ceu
 
+# enable magic sysrq key
+sudo sh -c 'echo "kernel.sysrq = 1" >> /etc/sysctl.d/99-sysctl.conf' 
+
+# install softwares
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
